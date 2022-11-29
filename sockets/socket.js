ec1a1bbfdfcc6f954bfb93f92ae115340222ca90
Andres Badillo <r.andres.badillo@gmail.com>
2022-11-29 13:29:31 -0500

    Se creo el servidor de sockets

---
+++ sockets/socket.js
@@ -0,0 +1,16 @@
+/* Mensajes de sockets */
+const {io} = require('../index');
+
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje', (payload) => {
+        console.log('mensaje:', payload);
+
+        io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+    })
+});
